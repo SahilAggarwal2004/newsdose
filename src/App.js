3035d826384f45195559b1790acefdf3ca4c0763
Sahil <aggarwalsahil2004@gmail.com>
2022-03-06 13:22:55 +0530

    reload bug fix

--- src/App.js
+++ src/App.js
@@ -15,18 +15,14 @@ export default class App extends Component { // This is a class based component
   constructor() {
     super()
     this.state = {
-      country: "in",
-      category: "",
+      country: localStorage.getItem('country') || "in",
       query: ""
     }
+    this.categories = ["", "Business", "Entertainment", "Health", "Science", "Sports", "Technology"];
   }
 
   setCoun = country => {
     this.setState({ country })
-  }
-
-  setCat = category => {
-    this.setState({ category })
   }
 
   search = query => {
@@ -48,8 +44,10 @@ export default class App extends Component { // This is a class based component
           }} />
           <Routes>
             {/* here key needs to defined uniquely for all categorized News components with different categories as when we switch to a different endpoint(category), react thinks that there is no need to render News again as its already rendered and does nothing but when we define different keys for different News components, so on changing the endpoint, it identifies that the new News component is different from the current one and it re-renders it with the new props */}
-            <Route path='/science' element={<News country={this.state.country} key={this.state.country + 'science' + this.state.query} category={'Science'} query={this.state.query} />} />
-            <Route path={`/${this.state.category.toLowerCase()}`} element={<News country={this.state.country} key={this.state.country + this.state.category + this.state.query} category={this.state.category} query={this.state.query} />} />
+            {this.categories.map((category)=>{
+              return <Route path={`/${category.toLowerCase()}`} element={<News country={this.state.country} key={this.state.country + category + this.state.query} category={category} query={this.state.query} />} />
+            })}
+            {/* <Route path='/science' element={<News country={this.state.country} key={this.state.country + 'science' + this.state.query} category={'Science'} query={this.state.query} />} /> */}
           </Routes>
         </Router>
       </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,9 +17,7 @@ export class Navbar extends Component {
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                 <div className="container-fluid">
                     {/* A way to use props in class based component described below, another by(by defining described in NewsItem.js) */}
-                    <Link className="navbar-brand" to="/" onClick={() => {
-                        this.props.setCat("")
-                    }}>News-Dose</Link>
+                    <Link className="navbar-brand" to="/">News-Dose</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -28,13 +26,14 @@ export class Navbar extends Component {
                             {this.categories.map((element) => {
                                 return (
                                     <li className="nav-item" key={element}>
-                                        <Link className="nav-link" aria-current="page" to={`/${element.toLowerCase()}`} onClick={() => {
-                                            this.props.setCat(element)
-                                        }}>{element ? element : "Home"}</Link>
+                                        <Link className="nav-link" aria-current="page" to={`/${element.toLowerCase()}`}>{element ? element : "Home"}</Link>
                                     </li>)
                             })}
                         </ul>
-                        <select className="form-select w-auto" aria-label="Default select example" defaultValue="in" onChange={() => { this.props.setCoun(document.querySelector("select").value) }} style={{ marginRight: "0.5rem" }}>
+                        <select className="form-select w-auto" aria-label="Default select example" defaultValue={localStorage.getItem('country') || "in"} onChange={() => {
+                            this.props.setCoun(document.querySelector("select").value);
+                            localStorage.setItem('country', document.querySelector("select").value)
+                        }} style={{ marginRight: "0.5rem" }}>
                             {this.countries.map(element => {
                                 return (
                                     <option value={element.split('_')[0]} key={element.split('_')[0]}>{element.split('_')[1]}</option>
