f440cdd55bc992c4e5c4d95313afff9c646ad2c4
Sahil <aggarwalsahil2004@gmail.com>
2022-03-06 21:44:17 +0530

    minor bug fix

--- src/App.js
+++ src/App.js
@@ -43,13 +43,13 @@ export default class App extends Component { // This is a class based component
           }} />
           <Routes>
             {/* here key needs to defined uniquely for all categorized News components with different categories as when we switch to a different endpoint(category), react thinks that there is no need to render News again as its already rendered and does nothing but when we define different keys for different News components, so on changing the endpoint, it identifies that the new News component is different from the current one and it re-renders it with the new props */}
-            <Route path='/' element={<News country={this.state.country} key='' category='' query={this.state.query} />} />
-            <Route path='/business' element={<News country={this.state.country} key='business' category='Business' query={this.state.query} />} />
-            <Route path='/entertainment' element={<News country={this.state.country} key='entertainment' category='Entertainment' query={this.state.query} />} />
-            <Route path='health' element={<News country={this.state.country} key='health' category='Health' query={this.state.query} />} />
-            <Route path='science' element={<News country={this.state.country} key='science' category='Science' query={this.state.query} />} />
-            <Route path='sports' element={<News country={this.state.country} key='sports' category='Sports' query={this.state.query} />} />
-            <Route path='technology' element={<News country={this.state.country} key='technology' category='Technology' query={this.state.query} />} />
+            <Route path='/' element={<News country={this.state.country} category='' query={this.state.query} key={`${this.state.country}${this.state.query}`} />} />
+            <Route path='/business' element={<News country={this.state.country} category='Business' query={this.state.query} key={`${this.state.country}business${this.state.query}`} />} />
+            <Route path='/entertainment' element={<News country={this.state.country} category='Entertainment' query={this.state.query} key={`${this.state.country}entertainment${this.state.query}`} />} />
+            <Route path='health' element={<News country={this.state.country} category='Health' query={this.state.query} key={`${this.state.country}health${this.state.query}`} />} />
+            <Route path='science' element={<News country={this.state.country} category='Science' query={this.state.query} key={`${this.state.country}science${this.state.query}`} />} />
+            <Route path='sports' element={<News country={this.state.country} category='Sports' query={this.state.query} key={`${this.state.country}sports${this.state.query}`} />} />
+            <Route path='technology' element={<News country={this.state.country} category='Technology' query={this.state.query} key={`${this.state.country}technology${this.state.query}`} />} />
           </Routes>
         </Router>
       </div>
