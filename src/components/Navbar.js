61f6f81c0990c577e14058ea53b53b4037c56a52
Sahil <aggarwalsahil2004@gmail.com>
2022-03-12 21:47:06 +0530

    web app modifications

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ export class Navbar extends Component {
             <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
                 <div className="container-fluid">
                     {/* A way to use props in class based component described below, another by(by defining described in NewsItem.js) */}
-                    <Link className="navbar-brand" to="/">News-Dose</Link>
+                    <Link className="navbar-brand" to="/">NewsDose</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
