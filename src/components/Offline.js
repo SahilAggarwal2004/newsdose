5afd679decfeb397e3aa5e3fe49fcb1905406c74
Sahil <aggarwalsahil2004@gmail.com>
2022-07-17 13:23:49 +0530

    offline page

--- src/components/Offline.js
+++ src/components/Offline.js
@@ -5,13 +5,13 @@ import { useNavigate } from 'react-router-dom';
 export default function Offline() {
     const redirect = useNavigate();
 
-    useEffect(() => { if (navigator.onLine) redirect('/dashboard') }, [navigator.onLine])
+    useEffect(() => { if (navigator.onLine) redirect('/') }, [navigator.onLine])
 
     document.title = 'You are Offline'
 
-    return <div className="w-screen h-screen flex justify-center items-center bg-purple-700 inset-0 fixed z-20">
-        <div className='text-white text-center px-4 font-sans space-y-2'>
-            <h1 className='text-3xl'>Offline...</h1>
+    return <div className="w-100 h-100 d-flex justify-content-center align-items-center top-0 start-0 position-fixed">
+        <div className='text-center px-4'>
+            <h2 className='mb-2'>Offline...</h2>
             <p>The current page isn't available offline. Please try again when you're back online.</p>
         </div>
     </div>
