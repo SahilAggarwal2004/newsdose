9c7bb04e4a9b1c1d7ac19dd422f38ffbd6c2be05
Sahil <aggarwalsahil2004@gmail.com>
2022-08-10 11:47:40 +0530

    rebuild

--- src/components/News.js
+++ src/components/News.js
@@ -37,8 +37,8 @@ export default function News({ category }) {
                 <input className="form-control w-auto" type="search" placeholder="Search" aria-label="Search" value={query} onChange={event => setQuery(event.target.value)} />
             </div>
             <hr />
-            <InfiniteScroll className="panel row mx-3 py-2 gx-4" next={() => fetchData(category, true, 'new')} hasMore={!end} loader={load[0] === 'visible' && <Loader />} endMessage={news.length && <p className='text-center fw-bold'>Yay! You have seen it all</p>} dataLength={fullNews.length}>
-                {Boolean(news.length) ? news.map(element => <div className="col-sm-6 col-lg-4 d-flex" key={element.url}>
+            <InfiniteScroll className="panel row mx-3 py-2 gx-4" next={() => fetchData(category, true, 'new')} hasMore={!end} loader={load[0] === 'visible' && <Loader />} endMessage={Boolean(news.length) && <p className='text-center fw-bold'>Yay! You have seen it all</p>} dataLength={fullNews.length}>
+                {news.length ? news.map(element => <div className="col-sm-6 col-lg-4 d-flex" key={element.url}>
                     <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
                 </div>) : category === 'saved' ? <div className="text-center">
                     You haven't saved any news till now!
--- src/components/Search.js
+++ src/components/Search.js
@@ -94,8 +94,8 @@ export default function Search() {
             </select>
         </div>
 
-        <InfiniteScroll className="panel row mt-3 mx-3 py-2 gx-4" next={() => searchBackend('new')} hasMore={!end} loader={load[0] === 'visible' && <Loader />} endMessage={searchNews.length && <p className='text-center fw-bold'>Yay! You have seen it all</p>} dataLength={searchNews.length}>
-            {Boolean(searchNews.length) ? searchNews.map(element => <div className="col-sm-6 col-lg-4 d-flex" key={element.url}>
+        <InfiniteScroll className="panel row mt-3 mx-3 py-2 gx-4" next={() => searchBackend('new')} hasMore={!end} loader={load[0] === 'visible' && <Loader />} endMessage={Boolean(searchNews.length) && <p className='text-center fw-bold'>Yay! You have seen it all</p>} dataLength={searchNews.length}>
+            {searchNews.length ? searchNews.map(element => <div className="col-sm-6 col-lg-4 d-flex" key={element.url}>
                 <NewsItem title={element.title} description={element.description} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
             </div>) : !query && load[0] === 'hidden' && country.code ? <div className="text-center">
                 Enter query to search for news...
