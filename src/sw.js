170ecfea7908072cf3e6d6e60c02fdd075abc423
Sahil <aggarwalsahil2004@gmail.com>
2022-10-18 21:00:40 +0530

    commit

--- src/sw.js
+++ src/sw.js
@@ -29,7 +29,12 @@ precacheAndRoute(urlsToCache)
 setDefaultHandler(new StaleWhileRevalidate())
 offlineFallback({ pageFallback: '/offline' });
 
-registerRoute(({ url, request }) => url.origin.includes('images.weserv.nl') || request.destination === 'image', new CacheFirst({
+registerRoute(({ url }) => url.pathname === '/news.webp', new CacheFirst({
+    cacheName: 'image',
+    plugins: [new CacheableResponsePlugin({ statuses: [200] })]
+}))
+
+registerRoute(({ url, request }) => url.pathname !== '/news.webp' && (url.origin.includes('images.weserv.nl') || request.destination === 'image'), new CacheFirst({
     cacheName: 'images',
     plugins: [
         new CacheableResponsePlugin({ statuses: [200] }),
