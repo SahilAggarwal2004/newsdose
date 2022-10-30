4a6af10d9cf29a0aeffdfd78f0a4fae54764b0ee
Sahil <aggarwalsahil2004@gmail.com>
2022-10-30 12:20:35 +0530

    rebuild

--- src/registersw.js
+++ src/registersw.js
@@ -3,7 +3,7 @@ import { Workbox } from "workbox-window";
 export default function registersw() {
     if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
         const wb = new Workbox('/sw.js')
-        wb.addEventListener('installed', event => { if (event.isUpdate) window.location.reload() })
+        wb.addEventListener('installed', event => { if (event.isUpdate && window.confirm('New update available, Click OK to refresh!')) window.location.reload() })
         wb.register()
     }
 }
