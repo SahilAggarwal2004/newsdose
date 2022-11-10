1a84b15902a780c5f163b8b9edbec4a920fab45c
Sahil <aggarwalsahil2004@gmail.com>
2022-11-10 20:22:35 +0530

    commit

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,17 +12,18 @@ export default function NewsItem(props) {
     const { setShareUrl } = useNewsContext()
     const showDate = dateFormat === 'UTC' ? new Date(date).toUTCString() : new Date(date).toLocaleString()
 
-    if (imgUrl?.match(/http/g)?.length !== 1) var newsImg = '/news.webp'
+    const newsImg = '/news.webp';
+    if (imgUrl?.match(/http/g)?.length !== 1) var imgUrlWeserv = newsImg
     else {
         const connectionSpeed = navigator.connection?.effectiveType
-        newsImg = `https://images.weserv.nl/?url=${imgUrl}&width=450&height=300&maxage=1d&output=webp&q=${connectionSpeed?.includes('2') ? 5 : connectionSpeed?.includes('3') ? 10 : 25}`
+        imgUrlWeserv = `https://images.weserv.nl/?url=${imgUrl}&width=450&height=300&maxage=1d&output=webp&q=${connectionSpeed?.includes('2') ? 5 : connectionSpeed?.includes('3') ? 10 : 25}`
     }
 
     return <div className="card mt-4 mb-3" style={{ paddingBottom: "2rem" }}>
         <span className="position-absolute top-0 start-50 translate-middle badge rounded-pill bg-danger border">{source}</span>
         <a href={newsUrl} target="_blank" rel="noreferrer" className="text-black">
             <div style={{ height: "13rem" }}>
-                <img src={newsImg} loading='lazy' className="card-img-top h-100" alt='' />
+                <img src={imgUrlWeserv} onError={event => event.target.src = newsImg} loading='lazy' className="card-img-top h-100" alt='' />
             </div>
         </a>
         <div className="card-body">
