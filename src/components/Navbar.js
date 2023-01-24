2a77bc7f1ac39184fad6ffb719c0739acd62cbc1
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-01-24 11:43:58 +0530

    commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import { useQueryClient } from '@tanstack/react-query';
 import { useEffect, useState } from 'react'
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar"
 import { countries, categories, pseudoCategories } from '../constants';
 import { useNewsContext } from '../context/ContextProvider';
@@ -10,7 +10,6 @@ export default function Navbar() {
     const { country: { method, code }, setCountry, progress, setProgress, queryFn } = useNewsContext()
     const client = useQueryClient()
     const [width, setWidth] = useState(window.outerWidth)
-    const navigate = useNavigate();
     const country = countries[code]
 
     useEffect(() => { window.addEventListener('resize', () => setWidth(window.outerWidth)); }, [])
@@ -23,7 +22,7 @@ export default function Navbar() {
     }
 
     function prefetch(event) {
-        const category = event.target.getAttribute("to").slice(1)
+        const category = event.target.pathname.slice(1)
         const queryKey = ['news', code, category]
         if (!pseudoCategories.includes(category)) client.prefetchInfiniteQuery({
             queryKey, retry: 0, enabled: method !== 'pending',
@@ -31,26 +30,20 @@ export default function Navbar() {
         })
     }
 
-    function redirect(event) {
-        event.preventDefault();
-        const path = event.target.getAttribute("to");
-        if (progress !== 33 && window.location.pathname !== path) navigate(path)
-    }
-
     return <>
         <LoadingBar color='#ff0000' progress={progress} waitingTime={300} onLoaderFinished={() => setProgress(0)} />
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <div className="container-fluid">
-                <a className="navbar-brand" to="/" onClick={redirect}>NewsDose</a>
+                <Link to="/" className="navbar-brand">NewsDose</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon" />
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav d-grid d-lg-flex me-auto mt-2 mb-2 mt-lg-0 mb-lg-0">
                         {categories.map(category => <li className={`nav-item text-center ${category}`} key={category}>
-                            <a className="nav-link d-inline-block w-auto" aria-current="page">
-                                <button className='btn shadow-none nav-link p-0 text-capitalize' data-bs-toggle='collapse' data-bs-target={width <= 991 && "#navbarSupportedContent"} to={`/${category}`} onMouseEnter={prefetch} onClick={redirect}>{category || "Home"}</button>
-                            </a>
+                            <Link to={`/${category}`} className="nav-link d-inline-block w-auto" aria-current="page" onMouseEnter={prefetch}>
+                                <button className='btn shadow-none nav-link p-0 text-capitalize' data-bs-toggle='collapse' data-bs-target={width <= 991 && "#navbarSupportedContent"}>{category || "Home"}</button>
+                            </Link>
                         </li>)}
                     </ul>
                     <select className="form-select w-auto" aria-label="Choose country" defaultValue={method || code} onChange={updateCountry}>
