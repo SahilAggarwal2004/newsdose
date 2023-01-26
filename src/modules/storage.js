4e0f957b8cbaa1b08db5a3178bdf01ef08a80200
Sahil <aggarwalsahil2004@gmail.com>
2023-01-26 18:48:13 +0530

    web storage bug fix

--- src/modules/storage.js
+++ src/modules/storage.js
@@ -1,11 +1,19 @@
 export const setStorage = (key, value, local = true) => (local ? localStorage : sessionStorage).setItem(key, JSON.stringify(value))
 
+export const removeStorage = (key, local = true) => (local ? localStorage : sessionStorage).removeItem(key)
+
 export const getStorage = (key, fallbackValue, local = true) => {
     let value = (local ? localStorage : sessionStorage).getItem(key)
-    if (value) value = JSON.parse(value)
-    else if (fallbackValue) {
-        value = fallbackValue
-        setStorage(key, value, local)
+    try {
+        value = JSON.parse(value)
+    } catch {
+        if (fallbackValue) {
+            value = fallbackValue
+            setStorage(key, value, local)
+        } else {
+            value = null
+            removeStorage(key, local)
+        }
     }
     return value
 }
