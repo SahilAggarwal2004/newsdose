82727265c4df23d9a9d0dcafb22766fe40d5438e
Sahil Aggarwal <aggarwalsahil2004@gmail.com>
2023-07-15 12:04:37 +0530

    commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,6 +6,7 @@ import { useDebounce, useStorage } from '../hooks'
 import { getStorage } from '../modules/storage'
 import Loader from './Loader'
 import NewsItem from './NewsItem'
+import { fallbackCount } from '../constants'
 
 export default function Search() {
     const { country: { code: country }, pending, error, queryFn, onSuccess, onError } = useNewsContext()
@@ -37,8 +38,8 @@ export default function Search() {
         </div>
 
         <InfiniteScroll className="panel row mt-3 mx-3 py-2 gx-4" next={fetchNextPage} hasMore={hasNextPage} loader={isFetching && <Loader />} endMessage={news.length > 0 && <p className='text-center fw-bold'>Yay! You have seen it all</p>} dataLength={news.length}>
-            {news.length ? news.map(item => <div className="col-sm-6 col-lg-4 d-flex" key={item.url}>
-                <NewsItem {...item} dateFormat='UTC' />
+            {news.length ? news.map((item, i) => <div className="col-sm-6 col-lg-4 d-flex" key={item.url}>
+                <NewsItem index={i % fallbackCount} dateFormat='UTC' {...item} />
             </div>) : !query ? <div className="text-center">
                 Enter query to search for news...
             </div> : query.length < 3 ? <div className="text-center">
