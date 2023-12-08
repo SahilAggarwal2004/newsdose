d352cc3af7dc72e4fe83314b7c93536eef4edba9
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2023-12-08 20:40:31 +0530

    commit

--- src/hooks/useURLState.js
+++ src/hooks/useURLState.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router"
 import { useSearchParams } from "next/navigation"
 
-export default function useURLState(param, defaultValue, replace = true) {
+export default function useURLState(param, defaultValue) {
     const router = useRouter()
     const searchParams = useSearchParams()
     const value = searchParams.get(param) || defaultValue
@@ -11,8 +11,7 @@ export default function useURLState(param, defaultValue, replace = true) {
         if (value) newParams.set(param, value)
         else newParams.delete(param)
         const url = router.pathname + '?' + newParams.toString()
-        if (replace) router.replace(url)
-        else router.push(url)
+        router.replace(url)
     }
 
     return [value, setValue]
