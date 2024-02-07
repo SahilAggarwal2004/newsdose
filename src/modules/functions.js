d3c8595285f3121100547b79301e6e5bc8ce0ea9
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-02-07 13:31:50 +0530

    commit

--- src/modules/functions.js
+++ src/modules/functions.js
@@ -1,9 +1,7 @@
 import { sign } from "jssign"
 import { getStorage } from "@/modules/storage"
 
-const getTime = Date.now
-
-export const genToken = () => sign(getTime(), process.env.NEXT_PUBLIC_SECRET)
+export const genToken = () => sign(Date.now(), process.env.NEXT_PUBLIC_SECRET)
 
 export function getFirstUrl(key, page) {
     const data = getStorage(key, undefined, key[0] === 'news')
