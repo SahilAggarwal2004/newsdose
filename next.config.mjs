677defeec542c4921b52e86c8ffc54faebccaf14
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2024-04-26 15:09:09 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
-import withPWAInit from "@serwist/next";
+import withSerwistInit from "@serwist/next";
 
 const pages = ["/", "/saved", "/search", "/_offline"];
 const news = ["0.webp", "1.webp", "2.webp", "3.webp", "4.webp", "5.webp", "6.webp", "7.webp", "8.webp", "9.webp"].map((news) => `/news/${news}`);
 const extras = ["/favicon.ico"];
-const revision = `${Date.now()}`;
+const revision = Date.now().toString();
 
-const withPWA = withPWAInit({
+const withPWA = withSerwistInit({
   swSrc: "src/sw.js",
   swDest: "public/sw.js",
   exclude: [/public\/sw.js/],
