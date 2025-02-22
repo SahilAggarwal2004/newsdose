472143e6aaf64b95bd4464c3e379fd2b2b886e66
SahilAggarwal2004 <aggarwalsahil2004@gmail.com>
2025-02-22 19:12:05 +0530

    [feat]: improve app update logic with version checks

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
+import fs from "fs";
 import withSerwistInit from "@serwist/next";
+import packageJSON from "./package.json" with { type: "json" };
 
 const pages = ["/", "/saved", "/search", "/_offline"];
 const news = ["0.webp", "1.webp", "2.webp", "3.webp", "4.webp", "5.webp", "6.webp", "7.webp", "8.webp", "9.webp"].map((news) => `/news/${news}`);
@@ -19,5 +21,21 @@ const nextConfig = {
     nextScriptWorkers: true,
   },
 };
+
+const manifestPath = "./public/manifest.json";
+
+const updateManifestVersion = () => {
+  if (fs.existsSync(manifestPath)) {
+    const manifest = JSON.parse(fs.readFileSync(manifestPath, "utf8"));
+    manifest.version = packageJSON.version;
+    manifest.id = packageJSON.version;
+    fs.writeFileSync(manifestPath, JSON.stringify(manifest, null, 2) + "\n");
+    console.log(`✅ Manifest version updated to ${packageJSON.version}`);
+  } else {
+    console.warn("⚠️  manifest.json not found!");
+  }
+};
+
+updateManifestVersion();
 
 export default withPWA(nextConfig);
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,13 +1,15 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useRouter } from "next/router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Script from "next/script";
 import { useEffect, useState } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import ContextProvider from "@/contexts/ContextProvider";
+
 import Navbar from "@/components/Navbar";
 import Modal from "@/components/Modal";
 import { hideNavbar } from "@/constants";
+import ContextProvider from "@/contexts/ContextProvider";
+import { handleVersionUpdate } from "@/modules/update";
 import "@/styles/globals.css";
 
 const client = new QueryClient({ defaultOptions: { queries: { staleTime: 600000 } } });
@@ -19,6 +21,9 @@ export default function RootLayout({ Component, pageProps }) {
 
   useEffect(() => {
     setLoading(false);
+    if ("serviceWorker" in navigator) {
+      window.serwist.register().then(handleVersionUpdate);
+    }
   }, []);
 
   return (
@@ -29,16 +34,30 @@ export default function RootLayout({ Component, pageProps }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <meta name="keywords" content="news, dose, newsdose, newsdoseweb, web, platform, quick, quick news, daily new, daily news, daily dose, news bites, free, vercel, reactjs, online, online platform, home, about, general, top, world, business, entertainment, environment, health, politics, science, sports, sports news, technology, access, anywhere, anytime, fast, independent, web app, world, provides" />
-        <meta name="description" content="NewsDose is an online platform which provides quick daily news bites for free. Interested in weather, politics, sports news, etc? NewsDose is here for you!" />
+        <meta
+          name="keywords"
+          content="news, dose, newsdose, newsdoseweb, web, platform, quick, quick news, daily new, daily news, daily dose, news bites, free, vercel, reactjs, online, online platform, home, about, general, top, world, business, entertainment, environment, health, politics, science, sports, sports news, technology, access, anywhere, anytime, fast, independent, web app, world, provides"
+        />
+        <meta
+          name="description"
+          content="NewsDose is an online platform which provides quick daily news bites for free. Interested in weather, politics, sports news, etc? NewsDose is here for you!"
+        />
         <link rel="manifest" href="/manifest.json" />
 
         <link rel="preconnect" href={process.env.NEXT_PUBLIC_API} />
 
         <link rel="apple-touch-icon" href="icons/apple-icon-180.png" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-2048-2732.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-2732-2048.jpg" media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-2048-2732.jpg"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-2732-2048.jpg"
+          media="(device-width: 1024px) and (device-height: 1366px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
         <link
           rel="apple-touch-startup-image"
           href="icons/apple-splash-1668-2388.jpg"
@@ -135,8 +154,16 @@ export default function RootLayout({ Component, pageProps }) {
           media="(device-width: 414px) and (device-height: 
 896px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
         />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-828-1792.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-1792-828.jpg" media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-828-1792.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-1792-828.jpg"
+          media="(device-width: 414px) and (device-height: 896px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
         <link
           rel="apple-touch-startup-image"
           href="icons/apple-splash-1242-2208.jpg"
@@ -149,13 +176,34 @@ export default function RootLayout({ Component, pageProps }) {
           media="(device-width: 414px) and (device-height: 
 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: landscape)"
         />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-750-1334.jpg" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-1334-750.jpg" media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-640-1136.jpg" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
-        <link rel="apple-touch-startup-image" href="icons/apple-splash-1136-640.jpg" media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)" />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-750-1334.jpg"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-1334-750.jpg"
+          media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-640-1136.jpg"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)"
+        />
+        <link
+          rel="apple-touch-startup-image"
+          href="icons/apple-splash-1136-640.jpg"
+          media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: landscape)"
+        />
       </Head>
 
-      <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossOrigin="anonymous" strategy="lazyOnload" />
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+        crossOrigin="anonymous"
+        strategy="lazyOnload"
+      />
 
       {/* Global site tag (gtag.js) - Google Analytics */}
       <Script src="https://www.googletagmanager.com/gtag/js?id=G-6WZPD076ZK" strategy="worker" />
